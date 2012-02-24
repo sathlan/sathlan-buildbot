c93a1ee6d1cfc64ecbbfc9a41d23fbd8520eeeff
sathlan <mypublicaddress-code@ymail.com>
2012-02-24 02:39:06 +0100

    Improving README.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,56 @@
 # == Class: buildbot
 #
-# Full description of class buildbot here.
+# This module manage builbot slave/master.
+#
+# Tested plateforms:
+#  - Debian 6.0 Squeeze
+#    - puppet 2.7.10
+#
+#
+# run "rake spec" to check that everything works for you.  Requires:
+#  - ruby 1.9
+#  - rspec-puppet
+#  - mocha
 #
 # === Parameters
 #
-# Document parameters here.
+# ==== For slave
+#  - $projects=[]                 : an array of projects this slave is used for.
+#  - $type='slave'                : type of configuration : master/slave
+#  - $ensure='running'            : running, stopped. TODO: only running works.
+#  - $user='bbslave'              : user used to run the bot.
+#  - $mail='buildbot@example.com' : mail of the administration.
+#  - $admin='admin'               : real name of the administration.
+#  - $master_ip='192.168.0.2',    : the master ip.
+#  - $master_pass='pass1',        : the used password.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# ==== For master: TODO: NOT WORKING YET
+#  $slaves=[],                  : an array of slaves.
+#  $passwords=[],               : the associated password.
+#  $gitsource='',               : the source of the project.
+#  $subproject_names=[],        : subproject.
+#  $proj_title='',              : name of the project.
+#  $url='',                     : url of the project.
+#  $slave_listen='',            : port where the slave will connect.
+#  $listen='',                  : port where the admin panel will listen.
+#  $try_scheduler=[],           : user/passwd for the try.
+#
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# No varible required.
 #
 # === Examples
 #
-#  class { buildbot:
-#    ...
-#  }
+# ==== For a slave
+# class buildbot(
+#        projects: ['compile_stuff'],
+#        user:    'buildbot',
+#        admin:   'alice',
+#        mail:    'alice@dot.com',
+#        master_ip: '192.168.0.8',
+#        master_pass: 'my_pass')
 #
 # === Authors
 #
@@ -33,8 +58,11 @@
 #
 # === Copyright
 #
-# Copyright 2011 Your name here, unless otherwise noted.
+# This software is Copyright (c) 2012 by sathlan.
 #
+# This is free software, licensed under:
+#
+#  The GNU Lesser General Public License, Version 3, June 2007
 class buildbot(
   $projects=[],
   $type='slave',
